01b4d1ba7fc0515a7e6cc27d61a9c239abc39159
unknown <rfdg125@yahoo.com>
2025-06-10 17:11:34 -0600

    DannyG

---
+++ L02_01_helloLED/src/L02_01_helloLED.cpp
@@ -0,0 +1,26 @@
+/* 
+ * Project myProject
+ * Author: Your Name
+ * Date: 
+ * For comprehensive documentation and examples, please visit:
+ * https://docs.particle.io/firmware/best-practices/firmware-template/
+ */
+
+// Include Particle Device OS APIs
+#include "Particle.h"
+
+// Let Device OS manage the connection to the Particle Cloud
+SYSTEM_MODE(AUTOMATIC);
+
+
+void setup() {
+  pinMode(D1, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(D1, HIGH);
+  delay(1000);
+  digitalWrite(D1, LOW);
+  delay(1000);
+}
---
+++ L02_02_helloLEDvar/src/L02_02_helloLEDvar.cpp
@@ -0,0 +1,25 @@
+/* 
+ * Project myProject
+ * Author: Your Name
+ * Date: 
+ * For comprehensive documentation and examples, please visit:
+ * https://docs.particle.io/firmware/best-practices/firmware-template/
+ */
+
+// Include Particle Device OS APIs
+#include "Particle.h"
+
+// Let Device OS manage the connection to the Particle Cloud
+SYSTEM_MODE(AUTOMATIC);
+
+// Run the application and system concurrently in separate threads
+
+void setup() {
+  
+}
+
+
+void loop() {
+
+}
+ 
